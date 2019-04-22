a7c24f2c2c6607a1ec19d272328a2abd1a9e9aa4
Andrew Fontaine <andrew@afontaine.ca>
2019-04-22 13:50:16 -0400

    Add Repo for Getting Feature Flags
    To avoid blocking on feature enabled computation, the Repo only
    retrieves flags. Computation should happen on the main thread.

--- lib/unleash/features.ex
+++ lib/unleash/features.ex
@@ -4,31 +4,29 @@ defmodule Unleash.Features do
   defstruct version: "1", features: []
 
   def from_map(map) when is_map(map) do
-    %__MODULE__{
-      version: map["version"],
-      features: Enum.map(map["features"], fn f -> Feature.from_map(f) end)
-    }
+    {:ok,
+     %__MODULE__{
+       version: map["version"],
+       features: Enum.map(map["features"], fn f -> Feature.from_map(f) end)
+     }}
   end
 
-  def from_map(_), do: %__MODULE__{}
+  def from_map(_), do: {:error, "failed to construct state"}
 
-  def enabled?(nil, _feature, _context), do: false
+  def get_feature(nil, _feat), do: %Feature{}
 
-  def enabled?(%__MODULE__{features: nil}, _feature, _context), do: false
+  def get_feature(%__MODULE__{features: nil}, _feature), do: %Feature{}
 
-  def enabled?(%__MODULE__{}, nil, _context), do: false
+  def get_feature(%__MODULE__{}, nil), do: %Feature{}
 
-  def enabled?(%__MODULE__{features: features} = state, feat, context)
-      when is_list(features) and is_atom(feat) do
-    enabled?(state, Atom.to_string(feat), context)
-  end
+  def get_feature(%__MODULE__{features: features} = state, feat)
+      when is_list(features) and is_atom(feat),
+      do: get_feature(state, Atom.to_string(feat))
 
-  def enabled?(%__MODULE__{features: features}, feat, context)
+  def get_feature(%__MODULE__{features: features}, feat)
       when is_list(features) and is_binary(feat) do
     features
-    |> IO.inspect()
     |> Enum.find(fn feature -> compare(feature.name, feat) end)
-    |> Feature.enabled?(context)
   end
 
   defp compare(n1, n2), do: String.downcase(n1) == String.downcase(n2)
---
+++ lib/unleash/repo.ex
@@ -0,0 +1,35 @@
+defmodule Unleash.Repo do
+  use GenServer
+
+  alias Unleash.Client
+  alias Unleash.Features
+
+  def init(_) do
+    {:ok, %Features{}}
+  end
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: Unleash.Repo)
+  end
+
+  def initialize() do
+    GenServer.cast(Unleash.Repo, :initialize)
+  end
+
+  def get_feature(name) do
+    GenServer.call(Unleash.Repo, {:get_feature, name})
+  end
+
+  def handle_call({:get_feature, name}, _from, state) do
+    feature = Features.get_feature(state, name)
+
+    {:reply, feature, state}
+  end
+
+  def handle_cast(:initialize, _state) do
+    case Client.features() do
+      {:ok, features} -> {:noreply, features}
+      {:error, r} -> {:stop, r}
+    end
+  end
+end
