78ac68f3646226dd777b4cdb4416011e778988a3
Andrew Fontaine <afontaine@gitlab.com>
2019-08-08 11:46:03 -0400

    Add `is_enabled?` to Closer Match Unleash Spec

--- lib/unleash.ex
+++ lib/unleash.ex
@@ -7,6 +7,14 @@ defmodule Unleash do
   alias Unleash.Metrics
   alias Unleash.Feature
 
+  @spec is_enabled?(atom() | String.t(), boolean) :: boolean
+  def is_enabled?(feature, default) when is_boolean(default),
+    do: enabled?(feature, default)
+
+  @spec is_enabled?(atom() | String.t(), Map.t(), boolean) :: boolean
+  def is_enabled?(feature, context \\ %{}, default \\ false),
+    do: enabled?(feature, context, default)
+
   @spec enabled?(atom() | String.t(), boolean) :: boolean
   def enabled?(feature, context) when is_boolean(context),
     do: enabled?(feature, %{}, context)
