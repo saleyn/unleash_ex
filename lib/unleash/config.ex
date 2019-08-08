74317a22b46fce5c5e3437f30b5ca8fafda247a4
Andrew Fontaine <afontaine@gitlab.com>
2019-08-08 11:46:33 -0400

    Refactor Config to Simplify Actual Configuration

--- lib/unleash/config.ex
+++ lib/unleash/config.ex
@@ -1,54 +1,55 @@
 defmodule Unleash.Config do
-  @url "http://localhost:4242"
-  @appname "test"
-  @instance_id "test"
-  @metrics_period 10 * 60 * 1000
-  @features_period 15 * 1000
-  @strategies Unleash.Strategies
+  @defaults [
+    url: "http://localhost:4242",
+    appname: "test",
+    instance_id: "test",
+    metrics_period: 10 * 60 * 1000,
+    features_period: 15 * 1000,
+    strategies: Unleash.Strategies
+  ]
 
   def url() do
-    application()
-    |> Application.get_env(:url, @url)
+    application_env()
+    |> Keyword.fetch!(:url)
   end
 
   def appname() do
-    application()
-    |> Application.get_env(:appname, @appname)
+    application_env()
+    |> Keyword.fetch!(:appname)
   end
 
   def instance_id() do
-    application()
-    |> Application.get_env(:instance_id, @instance_id)
+    application_env()
+    |> Keyword.fetch!(:instance_id)
   end
 
   def metrics_period() do
-    application()
-    |> Application.get_env(:metrics_period, @metrics_period)
+    application_env()
+    |> Keyword.fetch!(:metrics_period)
   end
 
   def features_period() do
-    application()
-    |> Application.get_env(:features_period, @features_period)
+    application_env()
+    |> Keyword.fetch!(:features_period)
   end
 
   def strategies() do
     strategy_module =
-      application()
-      |> Application.get_env(:strategies, @strategies)
+      application_env()
+      |> Keyword.fetch!(:strategies)
 
     strategy_module.strategies()
   end
 
   def strategy_names() do
-    strategy_module =
-      application()
-      |> Application.get_env(:strategies, @strategies)
-
-    strategy_module.strategies()
+    strategies()
     |> Enum.map(fn {n, _} -> n end)
   end
 
-  defp application() do
-    Application.get_application(__MODULE__)
+  defp application_env() do
+    __MODULE__
+    |> Application.get_application()
+    |> Application.get_env(Unleash, [])
+    |> Keyword.merge(@defaults)
   end
 end
