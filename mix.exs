398cbf456f89d2228f3506d9f1d55562ce95d53e
Andrew Fontaine <afontaine@gitlab.com>
2019-08-09 15:35:44 -0400

    Prepare for Release!

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Unleash.MixProject do
   def project do
     [
       app: :unleash,
-      version: "0.1.0",
+      version: "VERSION" |> File.read!() |> String.trim(),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
