5a1034f89d09bf91c9709c187d3a7b41b25c0d19
Andrew Fontaine <afontaine@gitlab.com>
2019-08-13 09:54:44 -0400

    Squashed 'priv/client-specification/' content from commit 8b81d34
    git-subtree-dir: priv/client-specification
    git-subtree-split: 8b81d34b0a4fa9a8b1699f1bdb4dc4ec3fcadd5f

---
+++ schema/test-case-schema.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+const featuresSchema = require('./features-schema.js');
+const contextSchema = require('./context-schema.js');
+
+const schema = Joi.object().keys({
+    name: Joi.string(),
+    state: featuresSchema,
+    tests: Joi.array().items(
+        Joi.object().keys({
+            description: Joi.string().required(),
+            context: contextSchema,
+            toggleName: Joi.string().required(),
+            expectedResult: Joi.boolean().required(),
+        })
+    ),
+    variantTests: Joi.array().items(
+        Joi.object().keys({
+            description: Joi.string().required(),
+            context: contextSchema,
+            toggleName: Joi.string().required(),
+            expectedResult: Joi.object().required().keys({
+                name: Joi.string().required(),
+                payload: Joi.object().required().keys({
+                    type: Joi.string().required(),
+                    value: Joi.string().required().allow(""),
+                }).optional(),
+                enabled: Joi.boolean().required()
+            }),
+        })
+    )
+});
+
+module.exports = schema;
