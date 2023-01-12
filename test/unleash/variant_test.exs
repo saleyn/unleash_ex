fb0704dcd6de0bc15bc514398ca91f4d4f60785c
Jekri Preclaro Orlina <jekripreclaroorlina@gmail.com>
2023-01-12 10:20:17 +0000

    Variant evaluation telemetry

--- test/unleash/variant_test.exs
+++ test/unleash/variant_test.exs
@@ -24,6 +24,112 @@ defmodule Unleash.VariantTest do
            } == Unleash.get_variant(:flag_without_variants)
   end
 
+  describe "get_variant telemetry" do
+    test "emits telemetry on start" do
+      attach_telemetry_event([:unleash, :variant, :get, :start])
+
+      Unleash.get_variant(:disabled_variant)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :disabled_variant
+
+      assert is_number(measurements[:system_time])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "emits telemetry with reason (flag without variants) on stop" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:flag_without_variants)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :flag_without_variants
+      assert metadata.reason === :feature_has_no_variants
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "emits telemetry with reason (inexistent featureflag) on stop" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:inexistent_feature_flag)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :inexistent_feature_flag
+      assert metadata.reason === :feature_not_found
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "emits telemetry with reason (disabled_variant) on stop" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:disabled_variant)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :disabled_variant
+      assert metadata.reason === :feature_disabled
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "emits weight selection reason on stop when applicable" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:weight_test)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :weight_test
+      assert metadata.reason === :variant_selected
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "emits override selection reason on stop when applicable" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:override_test, %{user_id: "420"})
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      assert metadata.variant === :override_test
+      assert metadata.reason === :override_found
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+
+    test "returns seed and variants on stop" do
+      attach_telemetry_event([:unleash, :variant, :get, :stop])
+
+      Unleash.get_variant(:weight_test)
+
+      assert_received {:telemetry_metadata, metadata}
+      assert_received {:telemetry_measurements, measurements}
+
+      refute is_nil(metadata.seed)
+      assert metadata.variants === [{"variant1", 99}, {"variant2", 1}]
+
+      assert is_number(measurements[:duration])
+      assert is_number(measurements[:monotonic_time])
+    end
+  end
+
   defp start_repo(_) do
     stop_supervised(Unleash.Repo)
 
@@ -33,10 +139,77 @@ defmodule Unleash.VariantTest do
     :ok
   end
 
+  defp attach_telemetry_event(event) do
+    test_pid = self()
+
+    :telemetry.attach(
+      make_ref(),
+      event,
+      fn
+        ^event, measurements, metadata, _config ->
+          send(test_pid, {:telemetry_measurements, measurements})
+          send(test_pid, {:telemetry_metadata, metadata})
+      end,
+      []
+    )
+  end
+
   defp state,
     do: %{
       "version" => 1,
       "features" => [
+        %{
+          "name" => "override_test",
+          "description" => "override testing",
+          "enabled" => true,
+          "strategies" => [
+            %{
+              "name" => "default",
+              "parameters" => %{}
+            }
+          ],
+          "variants" => [
+            %{
+              "name" => "variant1",
+              "weight" => 50,
+              "payload" => %{"type" => "string", "value" => "val1"},
+              "overrides" => [
+                %{
+                  "contextName" => "userId",
+                  "values" => ["420"]
+                }
+              ]
+            },
+            %{
+              "name" => "variant2",
+              "weight" => 50,
+              "payload" => %{"type" => "string", "value" => "val1"}
+            }
+          ]
+        },
+        %{
+          "name" => "weight_test",
+          "description" => "weight selection testing fixture",
+          "enabled" => true,
+          "strategies" => [
+            %{
+              "name" => "default",
+              "parameters" => %{}
+            }
+          ],
+          "variants" => [
+            %{
+              "name" => "variant1",
+              "weight" => 99,
+              "payload" => %{"type" => "string", "value" => "val1"}
+            },
+            %{
+              "name" => "variant2",
+              "weight" => 1,
+              "payload" => %{"type" => "string", "value" => "val1"}
+            }
+          ]
+        },
         %{
           "name" => "disabled_variant",
           "description" => "variant with enabled set to false",
