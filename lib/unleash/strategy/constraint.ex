9fa1712c75ec632768505130dcd4469b817967cf
Anatoly Rodionov <a.rodionov@samsung.com>
2025-05-15 15:08:09 -0400

    new constraint type added

--- lib/unleash/strategy/constraint.ex
+++ lib/unleash/strategy/constraint.ex
@@ -12,16 +12,20 @@ defmodule Unleash.Strategy.Constraint do
     Enum.all?(constraints, &verify(&1, context))
   end
 
-  defp verify(%{"contextName" => name, "operator" => op, "values" => values}, context) do
+  defp verify(%{"contextName" => name, "operator" => op, "inverted" => inverted} = constrain, context) do
     context
     |> find_value(name)
-    |> check(op, values)
+    |> check(op, constrain)
+    |> invert(inverted)
   end
 
   defp verify(%{}, _context), do: false
 
-  defp check(value, "IN", values), do: value in values
-  defp check(value, "NOT_IN", values), do: value not in values
+  defp check(value, "IN", %{"values" => values}), do: value in values
+  defp check(value, "NOT_IN", %{"values" => values}), do: value not in values
+  defp check(_, "DATE_AFTER", %{"value" => value}), do: compare_date(value) == :gt
+  defp check(_, "DATE_BEFORE", %{"value" => value}), do: compare_date(value) == :lt
+  defp check(nil, _, _), do: false
 
   defp find_value(nil, _name), do: nil
 
@@ -32,4 +36,21 @@ defmodule Unleash.Strategy.Constraint do
       find_value(Map.get(ctx, :properties), name)
     )
   end
+
+  defp invert(result, true), do: !result
+  defp invert(result, _), do: result
+
+  defp compare_date(nil), do: false
+
+  defp compare_date(value) do
+    case DateTime.from_iso8601(value) do
+      {:ok, date, _shift} ->
+        DateTime.utc_now()
+        |> DateTime.compare(date)
+
+      _ ->
+        false
+    end
+  end
+
 end
